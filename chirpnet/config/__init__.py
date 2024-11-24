44e7027c9dd1f4276ea603c62d63c5b5e6a361be
RobbeRDG <robbedegroeve@gmail.com>
2024-11-24 20:52:33 +0000

    refactor: move config to seperate folder

---
+++ chirpnet/config/__init__.py
@@ -0,0 +1,3 @@
+from chirpnet.config.config import Config
+
+Config = Config()
--- chirpnet/config/config.py
+++ chirpnet/config/config.py
@@ -1,17 +1,20 @@
 from pydantic_settings import BaseSettings
 
-
 class Config(BaseSettings):
     """
     A class to store the configuration of the Chirpnet project.
 
     Attributes
     ----------
+    PROJECT_BASE_DIR : str
+        The base directory of the Chirpnet project.
     DOWNLOADER_MAX_WORKERS : int
         The maximum number of workers to use in the chirpnet downloader when downloading
         species data.
 
     """
+
+    PROJECT_BASE_DIR: str = "/workspaces/chirpnet"
 
     # Chirpnet downloader configurations
     DOWNLOADER_MAX_WORKERS: int = 4
